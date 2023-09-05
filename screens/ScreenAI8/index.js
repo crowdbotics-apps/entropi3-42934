cdcd30938b21f9f19a9a449d40a0e09e12b02dba
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-09-05 19:20:48 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI8/index.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, Image, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+
+const MatchScreen = () => {
+  const [matches, setMatches] = useState([{
+    id: '1',
+    team1: 'Team A',
+    team2: 'Team B'
+  }, {
+    id: '2',
+    team1: 'Team C',
+    team2: 'Team D'
+  }, {
+    id: '3',
+    team1: 'Team E',
+    team2: 'Team F'
+  }]);
+
+  const MatchItem = ({
+    item
+  }) => <View style={styles.matchItem}>
+      <Image style={styles.teamLogo} source={{
+      uri: 'https://tinyurl.com/42evm3m3'
+    }} />
+      <Text style={styles.teamText}>{item.team1} VS {item.team2}</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Select Winner</Text>
+      </TouchableOpacity>
+    </View>;
+
+  return <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Parlay Matches</Text>
+      <FlatList data={matches} renderItem={MatchItem} keyExtractor={item => item.id} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Betting History</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Bet</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 20
+  },
+  matchItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd'
+  },
+  teamLogo: {
+    width: 50,
+    height: 50,
+    marginRight: 10
+  },
+  teamText: {
+    flex: 1,
+    fontSize: 18
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+    marginTop: 20
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16
+  }
+});
+export default MatchScreen;
---
+++ screens/ScreenAI9/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Image, Button } from 'react-native';
+
+const BettingRulesScreen = ({
+  navigation
+}) => {
+  return <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Betting Rules</Text>
+        <Button title="Close" onPress={() => navigation.goBack()} />
+      </View>
+      <View style={styles.content}>
+        <Image style={styles.image} source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} />
+        <Text style={styles.text}>
+          Rule 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </Text>
+        <Text style={styles.text}>
+          Rule 2: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+        <Text style={styles.text}>
+          Rule 3: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </Text>
+      </View>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5'
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#FFFFFF'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  content: {
+    padding: 20
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 10
+  }
+});
+export default BettingRulesScreen;
